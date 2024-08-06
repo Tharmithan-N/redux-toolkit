d5d73176208c87a8305adac0aa55f4097abcc4a5
Tharmithan <tharmithan@orelit.com>
2024-08-06 07:51:23 +0530

    crud redux added

--- redux-todo-app/src/App.js
+++ redux-todo-app/src/App.js
@@ -1,88 +1,15 @@
-// import "./App.css";
-// import React, { useState } from "react";
-// import { MdOutlineModeEdit } from "react-icons/md";
-// import { MdDeleteOutline } from "react-icons/md";
-// import { useSelector, useDispatch } from "react-redux";
-// import { addTodo } from "./redux/slice";
-
-// function App() {
-//   let [value, setValue] = useState();
-//   const dispatch = useDispatch();
-//   const todos = useSelector((state) => state.todos.todos);
-//   const handleChange = (e) => {
-//     setValue(e.target.value);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault(); // Prevent default form submission
-//     if (value.trim()) {
-//       dispatch({ type: "addTodo", payload: value });
-//       setValue(""); // Clear the input field after submission
-//     }
-//   };
-
-//   return (
-//     <div className="flex justify-center">
-//       <div>
-//         <div className="text-[36px] font-[600] text-center">
-//           Add your daily task here
-//         </div>
-//         <div className="pt-[20px]">
-//           <form onSubmit={handleSubmit}>
-//             <input
-//               type="text"
-//               className="border-[1px] rounded-[5px] w-[350px] px-[10px] py-[7px] mr-[10px]"
-//               onChange={handleChange}
-//               value={value} //need to bind the value of this field to the state
-//             />
-//             <button
-//               type="submit"
-//               className="text-[18px] px-[10px] py-[5px] bg-blue-500 text-center rounded-md text-white"
-//             >
-//               Add
-//             </button>
-//           </form>
-//         </div>
-//         {todos.map((todo) => {
-//           return (
-//             <div
-//               className="flex gap-[5px] mt-[20px] justify-between align-middle py-[10px] rounded-lg bg-[#4e4d4da8]"
-//               key={todo}
-//             >
-//               <div className="text-[18px] px-[10px] py-[5px] text-[#cbc9c9]">
-//                 {todo}
-//               </div>
-//               <div className="flex gap-[10px] mr-[10px]">
-//                 <div className="text-[18px] px-[10px] py-[5px] bg-green-500 text-center rounded-md text-white flex items-center justify-center">
-//                   <MdOutlineModeEdit className="mx-auto" />
-//                 </div>
-//                 <div className="text-[18px] px-[10px] py-[5px] bg-red-500 text-center rounded-md text-white flex items-center justify-center">
-//                   <MdDeleteOutline className="mx-auto" />
-//                 </div>
-//               </div>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import "./App.css";
 import React, { useState } from "react";
 import { MdOutlineModeEdit } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 import { useSelector, useDispatch } from "react-redux";
-import { addTodo } from "./redux/slice";
+import { addTodo, deleteTodo, editTodo } from "./redux/slice";
 
 function App() {
   let [value, setValue] = useState("");
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos.todos);
+  const [editingIndex, setEditingIndex] = useState(null);
 
   const handleChange = (e) => {
     setValue(e.target.value);
@@ -94,6 +21,22 @@ function App() {
       dispatch(addTodo(value));
       setValue("");
     }
+  };
+
+  const handleEdit = (index) => {
+    const todo = todos[index];
+    setEditingIndex(index);
+    setValue(todo);
+  };
+
+  const handleUpdate = (index) => {
+    dispatch(editTodo({ index, value: value.trim() }));
+    setEditingIndex(null);
+    setValue("");
+  };
+
+  const handleDelete = (index) => {
+    dispatch(deleteTodo(index));
   };
 
   return (
@@ -109,6 +52,7 @@ function App() {
               className="border-[1px] rounded-[5px] w-[350px] px-[10px] py-[7px] mr-[10px]"
               onChange={handleChange}
               value={value}
+              autoFocus={editingIndex === null}
             />
             <button
               type="submit"
@@ -124,13 +68,30 @@ function App() {
             key={index} // Use index or a unique id if available
           >
             <div className="text-[18px] px-[10px] py-[5px] text-[#cbc9c9]">
-              {todo}
+              {editingIndex === index ? (
+                <input
+                  type="text"
+                  className="border-[1px] rounded-[5px] w-[350px] px-[10px] py-[7px] mr-[10px]"
+                  onChange={handleChange}
+                  value={value}
+                  onBlur={() => handleUpdate(index)}
+                  autoFocus
+                />
+              ) : (
+                todo
+              )}
             </div>
             <div className="flex gap-[10px] mr-[10px]">
-              <div className="text-[18px] px-[10px] py-[5px] bg-green-500 text-center rounded-md text-white flex items-center justify-center">
+              <div
+                className="text-[18px] px-[10px] py-[5px] bg-green-500 text-center rounded-md text-white flex items-center justify-center"
+                onClick={() => handleEdit(index)}
+              >
                 <MdOutlineModeEdit className="mx-auto" />
               </div>
-              <div className="text-[18px] px-[10px] py-[5px] bg-red-500 text-center rounded-md text-white flex items-center justify-center">
+              <div
+                className="text-[18px] px-[10px] py-[5px] bg-red-500 text-center rounded-md text-white flex items-center justify-center"
+                onClick={() => handleDelete(index)}
+              >
                 <MdDeleteOutline className="mx-auto" />
               </div>
             </div>
